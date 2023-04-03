503c08ed291d0add8d0ab53410793982f7d5f015
marissadreyfus <marissadreyfus@gmail.com>
2023-04-03 18:46:06 +0000

    did a thing, not done yet

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,19 +16,19 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [ //using these to actually test the items before actually putting in the actual positions. x-positions will be adjusted
-                { "type": "obstacle", "x": 1000, "y": groundY - 110, "damage": 10, "image": "img/star.png", "canBreak": false, "rotateVelocity": -5, "obstScaleX": 1.7, "obstScaleY": 1.7  },
-                { "type": "obstacle", "x": 1100, "y": groundY - 110, "damage": 5, "image": "img/star.png", "canBreak": false, "rotateVelocity": 0, "obstScaleX": 1.7, "obstScaleY": 1.7  },
-                { "type": "obstacle", "x": 1200, "y": groundY - 110, "damage": 30, "image": "img/star.png", "canBreak": false, "rotateVelocity": -2, "obstScaleX": 1.7, "obstScaleY": 1.7  },
-                { "type": "obstacle", "x": 1300, "y": groundY, "damage": 100, "image": "img/star.png", "canBreak": true, "rotateVelocity": 0, "obstScaleX": 1.7, "obstScaleY": 1.7  },
+                { "type": "obstacle", "x": 1000, "y": groundY - 110, "damage": 10, "image": "img/star.png", "rotateVelocity": -5, "obstScaleX": 1.7, "obstScaleY": 1.7  },
+                { "type": "obstacle", "x": 1100, "y": groundY - 110, "damage": 5, "image": "img/star.png", "rotateVelocity": 0, "obstScaleX": 1.7, "obstScaleY": 1.7  },
+                { "type": "obstacle", "x": 1200, "y": groundY - 110, "damage": 30, "image": "img/star.png", "rotateVelocity": -2, "obstScaleX": 1.7, "obstScaleY": 1.7  },
+                { "type": "obstacle", "x": 1300, "y": groundY, "damage": 100, "image": "img/star.png", "rotateVelocity": 0, "obstScaleX": 1.7, "obstScaleY": 1.7  },
 
                 { "type": "enemy", "x": 1400, "y": groundY - 50, "image": "img/alien_in_ufo.png", "healthLost": -25, "scoreAdded": 50, "enemyScaleX": 0.5, "enemyScaleY": 0.5  },
                 { "type": "enemy", "x": 1500, "y": groundY - 40, "image": "img/star.png", "healthLost": -100, "scoreAdded": 1000, "enemyScaleX": 0.5, "enemyScaleY": 0.5 },
                 { "type": "enemy", "x": 1600, "y": groundY - 50, "image": "img/star.png", "healthLost": -10, "scoreAdded": 10, "enemyScaleX": 0.5, "enemyScaleY": 0.5   },
                 { "type": "enemy", "x": 1700, "y": groundY - 40, "image": "img/star.png", "healthLost": -25, "scoreAdded": 25, "enemyScaleX": 1.7, "enemyScaleY": 1.7   },
 
-                { "type": "reward", "x": 1800, "y": groundY - 100, "image": "img/star.png", "pointsGiven": 25, "healthGiven": 10, "rewardScaleX": 0.5, "rewardScaleY": 0.5  },
-                { "type": "reward", "x": 1900, "y": groundY - 100, "image": "img/star.png", "pointsGiven": 25, "healthGiven": 10, "rewardScaleX": 0.5, "rewardScaleY": 0.5 },
-                { "type": "reward", "x": 2000, "y": groundY - 100, "image": "img/star.png", "pointsGiven": 25, "healthGiven": 10, "rewardScaleX": 0.5, "rewardScaleY": 0.5 },
+                { "type": "reward", "x": 1800, "y": groundY - 100, "image": "img/pink_orb.png", "pointsGiven": 15, "healthGiven": 15, "rewardScaleX": 0.5, "rewardScaleY": 0.5  },
+                { "type": "reward", "x": 1900, "y": groundY - 100, "image": "img/blue_orb.png", "pointsGiven": 30, "healthGiven": 25, "rewardScaleX": 0.5, "rewardScaleY": 0.5 },
+                { "type": "reward", "x": 2000, "y": groundY - 100, "image": "img/green_orb.png", "pointsGiven": 0, "healthGiven": 50, "rewardScaleX": 0.5, "rewardScaleY": 0.5 },
                 { "type": "reward", "x": 2100, "y": groundY - 100, "image": "img/medal_thing.png", "pointsGiven": 25, "healthGiven": 10, "rewardScaleX": 0.5, "rewardScaleY": 0.5 },
             ]
         };
@@ -56,15 +56,15 @@ var level01 = function (window) {
 
             if (canBreak === true) {
                 createObstacle.onProjectileCollision = function () {
-                    createObstacle.fadeOut(); 
+                    createObstacle.fadeOut(); //removes obstacle if hit by projectile
                 }
             } //if the object can "break" or not
             if (image === "img/star.png") {
                 hitZoneSize = 50;
-            }
+            }//changes hitzone of specific enemy
         }
 
-        function createEnemy (x, y, image, healthLost, scoreAdded, canShoot, enemyScaleX, enemyScaleY) {
+        function createEnemy (x, y, image, healthLost, scoreAdded, enemyScaleX, enemyScaleY) {
             var enemy = game.createGameItem("enemy", 25); //creates enemy
             var gameEnemy = draw.bitmap(image);//creates singular enemy
             gameEnemy.x = -25; //enemy image x-value
@@ -74,38 +74,16 @@ var level01 = function (window) {
             enemy.x = x; //enemy x-value
             enemy.y = y; //enemy y-value
             enemy.addChild(gameEnemy); //adds gameEnemy as a child of enemy
-            game.addGameItem(enemy);
-            enemy.velocityX = -2;
+            game.addGameItem(enemy);//adds enemy to game
+            enemy.velocityX = -2;//enemy velocity
             enemy.onPlayerCollision = function () {
-                game.changeIntegrity(healthLost)
-                enemy.fadeOut();
-            }
+                game.changeIntegrity(healthLost);//lowers health
+                enemy.fadeOut();//removes enemy
+            }//what happens when player collides with enemy
             enemy.onProjectileCollision = function () {
-                game.increaseScore(scoreAdded);
-                enemy.fadeOut();
-            }
-            function createEnemyProjectile () {
-                var enemyProjectile = game.createGameItem("enemy", 5);
-                var gameEnemyProjectile = draw.bitmap(image);
-                gameEnemyProjectile.x = -10;
-                gameEnemyProjectile.y = -10;
-                enemyProjectile.x = x * 3;
-                enemyProjectile.y = y;
-                enemyProjectile.addChild(gameEnemyProjectile);
-                game.addGameItem(enemyProjectile);
-                enemyProjectile.velocityX = -6;
-                gameEnemyProjectile.scaleX = 0.5;
-                gameEnemyProjectile.scaleY = 0.5;
-                enemyProjectile.onPlayerCollision = function () {
-                    game.changeIntegrity(-5);
-                    enemy.fadeOut();
-                } 
-            }//since this is the only enemy projectile, the numbers are hard-coded (i didn't feel like making even more variables)
-            if (canShoot === true) {
-                for (i = 0; i < 10; i++) {
-                    createEnemyProjectile();
-                }
-            } 
+                game.increaseScore(scoreAdded);//increases score
+                enemy.fadeOut();//removes enemy
+            }//when enemy is shot
         }
         function createReward (x, y, image, pointsGiven, healthGiven, rewardScaleX, rewardScaleY) {
             var reward = game.createGameItem("reward", 25); //creates reward
@@ -130,13 +108,13 @@ var level01 = function (window) {
                 var gameItem = levelData.gameItems[i];
 
                 if (gameItem.type === "obstacle") {
-                    createObstacle(gameItem.x, gameItem.y, gameItem.damage, gameItem.image, gameItem.canBreak, gameItem.rotateVelocity, gameItem.obstScaleX, gameItem.obstScaleY);
+                    createObstacle(gameItem.x, gameItem.y, gameItem.damage, gameItem.image, gameItem.canBreak, gameItem.rotateVelocity, gameItem.obstScaleX, gameItem.obstScaleY); //iterates over each object if the type is "obstacle"
                 } else if (gameItem.type === "enemy") {
-                    createEnemy(gameItem.x, gameItem.y, gameItem.image, gameItem.healthLost, gameItem.scoreAdded, gameItem.canShoot, gameItem.enemyScaleX, gameItem.enemyScaleY);
+                    createEnemy(gameItem.x, gameItem.y, gameItem.image, gameItem.healthLost, gameItem.scoreAdded, gameItem.enemyScaleX, gameItem.enemyScaleY);//iterates over each object if the type is "enemy"
                 } else if (gameItem.type === "reward") {
-                    createReward(gameItem.x, gameItem.y, gameItem.image, gameItem.pointsGiven, gameItem.healthGiven, gameItem.rewardScaleX, gameItem.rewardScaleY);
+                    createReward(gameItem.x, gameItem.y, gameItem.image, gameItem.pointsGiven, gameItem.healthGiven, gameItem.rewardScaleX, gameItem.rewardScaleY);//iterates over each object if the type is "reward"
                 }
-            }
+            }//iterates over each value in the object
 
         // DO NOT EDIT CODE BELOW HERE
     }
