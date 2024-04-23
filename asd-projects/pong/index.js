f563747d29d7617a5d7aa908e6ff9826bd95245d
marissadreyfus <marissadreyfus@gmail.com>
2024-04-23 17:17:25 +0000

    finished css for real this time, did newFrame & attached helper functions, added player and ball vars, THE PADDLES MOVE NOWgit add .!

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -19,21 +19,26 @@ function runProgram(){
   }
   
   // Game Item Objects
-  function gameItem (id, leftOrRight, topOrBottom) { //obj should send an id.
+  function gameItem (id, leftOrRight) { 
     return {
       id: id,
       x: parseFloat($(id).css(leftOrRight)),
-      y: parseFloat($(id).css(topOrBottom)),
-      speedX: 0,
+      y: parseFloat($(id).css('top')),
+      speedX: 0, //this will not be used for the paddles, but I need it for the ball to move 
       speedY: 0,
       width: $(id).width(),
       height: $(id).height(),
     };
   }
 
+  var p1 = gameItem('#leftPaddle', 'left');
+  var p2 = gameItem('#rightPaddle', 'right');
+  var ball = gameItem('#ball', 'left');
+
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -44,21 +49,53 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    reposGameItem(p1);
+    reposGameItem(p2);
+    reposGameItem(ball);
+    redraw(p1, 'left');
+    redraw(p2, 'right');
+    redraw(ball, 'left');
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    if (event.which === KEYCODES.W) {
+      p1.speedY = -5;
+    }
+    if (event.which === KEYCODES.S) {
+      p1.speedY = 5;
+    }
+    if (event.which === KEYCODES.UP) {
+      p2.speedY = -5;
+    }
+    if (event.which === KEYCODES.DOWN) {
+      p2.speedY = 5;
+    }
+  }
 
+  function handleKeyUp (event) {
+    if (event.which === KEYCODES.W || event.which || KEYCODES.S) {
+      p1.speedY = 0;
+    }
+    if (event.which === KEYCODES.UP || event.which || KEYCODES.DOWN) {
+      p2.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function reposGameItem (obj) {
+    obj.x += obj.speedX;
+    obj.y += obj.speedY;
+  }
+  function redraw (obj, leftOrRight) {
+    $(obj.id).css(leftOrRight, obj.x);
+    $(obj.id).css('top', obj.y);
+  }
   
   function endGame() {
     // stop the interval timer
@@ -67,5 +104,5 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
