91895b83eb8bbd45b2f1e18ade2ce4e339951dda
marissadreyfus <marissadreyfus@gmail.com>
2024-04-27 22:02:49 +0000

    doCollide works!!!!!!!!!!!!! named isColliding, might change where i put it but it works!!!!!!!!!!!!!!!!

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -120,10 +120,20 @@ function runProgram(){
     if (obj.y >= BOARD_HEIGHT - obj.height || obj.y <= 0) {
       obj.speedY = -obj.speedY;
     }
+    if (isColliding(obj, p1) === true || isColliding(obj, p2) === true) {
+      obj.speedX = -obj.speedX;
+    }
   }
   function boundPlayers (obj) {
     if (obj.y < 0 || obj.y > BOARD_HEIGHT - obj.height) {
       obj.y -= obj.speedY;
+    }
+  }
+  function isColliding (obj1, obj2) {
+    if (obj1.x < obj2.x + obj2.width && obj1.x + obj1.width > obj2.x && obj1.y < obj2.y + obj2.height && obj1.y + obj1.height > obj2.y) {
+      return true;
+    } else {
+      return false;
     }
   }
   function scoring () {
