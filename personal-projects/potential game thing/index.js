1ca6d0ed69c39190073cf5b23371633683566b74
marissadreyfus <marissadreyfus@gmail.com>
2024-05-08 15:49:01 +0000

    personal project, css works now and js is not empty anymore, full debrief in html i do not wanna retype all that

---
+++ personal-projects/potential game thing/index.js
@@ -0,0 +1,36 @@
+$(document).ready(runProgram);
+
+function runProgram () {
+    var FRAME_RATE = 60; //fps goes crazy//
+    var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+    //var BOARD_WIDTH = $("#board").width(); //sets the board width//
+    //var BOARD_HEIGHT = $("#board").height(); //sets the board height//
+    var KEYCODES = {
+        ENTER: 13,
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        KILL: 82,
+    }
+    var playerWithoutNameCurrently = {
+        x: 0,
+        y: 0,
+        speedX: 0,
+        speedY: 0,
+        width: $('#player').width(),
+        height: $('#player').height(),
+    }
+    var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);
+    $(document).on('keydown', handleKeyDown);
+    $(document).on('keyup', handleKeyUp);
+    function newFrame () {
+
+    }
+
+    function endGame() {
+        clearInterval(interval);
+        location.reload();
+        $(document).off();
+      }
+}
